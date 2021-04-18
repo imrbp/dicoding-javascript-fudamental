5bc5af99140b82a95515d9979428e3ef9078ff07
I Made Raditya Bayu P <im.raditya.bayu@apps.ipb.ac.id>
2021-04-19 00:40:35 +0700

    add concurrency

---
+++ 08_concurrency/06_async-await.js
@@ -0,0 +1,27 @@
+const getCoffee = () => {
+    return new Promise((resolve, reject) => {
+        const seeds = 100;
+        setTimeout(() => {
+            if (seeds >= 10) {
+                resolve("Kopi didapatkan!");
+            } else {
+                reject("Biji kopi habis!");
+            }
+        }, 1000);
+    })
+}
+
+async function makeCoffee() {
+    const coffee = await getCoffee();
+    console.log(coffee);
+}
+ 
+makeCoffee();
+ 
+/* output
+Kopi didapatkan!
+*/
+
+// await -> menunggu sebelum fungsi selanjutnya dijalankan;
+
+// async -> mengubah fungsi menjadi fungsi async
