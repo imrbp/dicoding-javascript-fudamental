22fa13c73cdfc2d615e088a83f11480ee1891725
I Made Raditya Bayu P <im.raditya.bayu@apps.ipb.ac.id>
2021-04-20 12:38:01 +0700

    add materi Node package Manager and kuis promise

--- 08_concurrency/kuis_concurrency.js
+++ 08_concurrency/kuis_concurrency.js
@@ -35,25 +35,30 @@ class NetworkError extends Error {
 }
 
 // TODO: 1
-const fetchingUserFromInternet = (callback, isOffline) => {
-  setTimeout(() => {
-    if (isOffline) {
-      callback(new NetworkError('Gagal mendapatkan data dari internet'), null);
-    }
-    callback(null, { name: 'John', age: 18 });
-  }, 500);
+const fetchingUserFromInternet = (isOffline) => {
+  return new Promise((resolve, reject) =>{
+    setTimeout(() => {
+      if (isOffline) {
+        reject(new NetworkError('Gagal mendapatkan data dari internet'));
+      } else {
+        resolve({ name: 'John', age: 18 });
+      }
+    }, 500);
+
+  })
 };
 
 
 // TODO: 2
-const gettingUserName = () => {
-  fetchingUserFromInternet((error, user) => {
-    if (error) {
-      return error.message;
+async function gettingUserName() {
+  try {
+      const user = await fetchingUserFromInternet(false); 
+      return user.name;
+    } catch(error) {
+      console.log(error);
     }
-    return user.name;
-  }, false);
-};
+}
+gettingUserName();
 
 /**
  * Hiarukan kode di bawah ini
