8b9e06029fd66bb7948c0e05854f7a9c77779996
I Made Raditya Bayu P <im.raditya.bayu@apps.ipb.ac.id>
2021-04-20 09:42:45 +0700

    add async and await file

--- 08_concurrency/06_async-await.js
+++ 08_concurrency/06_async-await.js
@@ -1,3 +1,7 @@
+
+// await -> menunggu sebelum fungsi selanjutnya dijalankan;
+
+// async -> mengubah fungsi menjadi fungsi async
 const getCoffee = () => {
     return new Promise((resolve, reject) => {
         const seeds = 100;
@@ -22,6 +26,52 @@ makeCoffee();
 Kopi didapatkan!
 */
 
-// await -> menunggu sebelum fungsi selanjutnya dijalankan;
 
-// async -> mengubah fungsi menjadi fungsi async
+// async-await pada promise
+async function makeCoffee() {
+    try {
+        const coffee = await getCoffee();
+        console.log(coffee);
+    } catch (rejectedReason) {
+        console.log(rejectedReason);
+    }
+}
+ 
+makeCoffee();
+ 
+/* output
+Biji kopi habis!
+*/
+
+// async-await pada chaining promise
+async function makeEspresso() {
+    try {
+        await checkAvailability();
+        await checkStock();
+        const coffee = await brewCoffee();
+        console.log(coffee);
+    } catch (rejectedReason) {
+        console.log(rejectedReason);
+    }
+}
+ 
+makeEspresso();
+ 
+/* output
+Membuatkan kopi Anda...
+Kopi sudah siap!
+*/
+
+
+// async-await pada promiseAll
+async function makeEspresso() {
+    try {
+        await checkAvailability();
+        await checkStock();
+        await Promise.all([boilWater(), grindCoffeeBeans()]);
+        const coffee = await brewCoffee();
+        console.log(coffee);
+    } catch (rejectedReason) {
+        console.log(rejectedReason);
+    }
+}
